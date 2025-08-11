74e72c7fe55b1aa433e90aac3f891e7f14f59eda
zzuhann <zzuhanlin@gmail.com>
2025-08-11 20:30:36 +0800

    feat: 移除 ImageService 中的 CacheControl 設定，取消 7 天快取以調整圖片加載策略

--- src/services/imageService.ts
+++ src/services/imageService.ts
@@ -79,7 +79,6 @@ export class ImageService {
         Body: file.buffer,
         ContentType: file.mimetype,
         ContentLength: file.size,
-        CacheControl: 'public, max-age=604800', // 7天快取 (7 * 24 * 60 * 60)
       };
 
       // 上傳到 R2
