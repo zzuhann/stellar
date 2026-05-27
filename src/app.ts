fdc626e0e399619740ca55008687e1d9a6669deb
zzuhann <zzuhanlin@gmail.com>
2026-05-28 04:53:50 +0800

    fix: add headers

--- src/app.ts
+++ src/app.ts
@@ -96,7 +96,7 @@ app.use(
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'x-request-id', 'x-session-id'],
   })
 );
 
