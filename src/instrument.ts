7dcbcbe19cea4beedb4487a528440c3292b54eae
zzuhann <zzuhanlin@gmail.com>
2026-05-30 07:24:42 +0800

    chore: only enabled when in production

--- src/instrument.ts
+++ src/instrument.ts
@@ -2,5 +2,6 @@ import * as Sentry from '@sentry/node';
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
+  enabled: process.env.NODE_ENV === 'production',
   tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1,
 });
